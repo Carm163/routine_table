b3f1a71471bb70069727e5976ec536ba9014edc8
Carm163 <irina.9146@gmail.com>
2025-12-11 15:41:53 +0400

    little mistake

--- playwright_tests/playwright.config.ts
+++ playwright_tests/playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   expect: { timeout: 5000 },
   reporter: [['list'], ['html', { open: 'never' }]],
   use: {
-    headless: true, // в CI: true, при локальной отладке ставь false
+    headless: true, // в CI: true, при локальной отладке false
     viewport: { width: 1280, height: 800 },
     actionTimeout: 5_000,
     baseURL: process.env.BASE_URL || 'http://localhost:8000', // или http://localhost (в зависимости от nginx)
@@ -17,6 +17,6 @@ export default defineConfig({
   },
   projects: [
     { name: 'chromium', use: { browserName: 'chromium' } },
-    // можно добавить firefox/webkit при необходимости
+    // firefox/webkit
   ],
 });
